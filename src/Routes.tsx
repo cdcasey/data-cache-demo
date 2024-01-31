2ea37bbe3242e50fc60b7537267e17390c7e22da
Chris Casey <cdcasey@users.noreply.github.com>
2024-01-30 21:55:43 -0600

    Remove error component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import ErrorPage from "./ErrorPage.tsx";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
 ]);
