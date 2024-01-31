8db1a614b00e7c1c1cfc32eb29f88cd4fbf1658a
Chris Casey <cdcasey@users.noreply.github.com>
2024-01-30 22:38:55 -0600

    Some data loading

--- src/People.tsx
+++ src/People.tsx
@@ -1,3 +1,19 @@
+import { useParams } from "react-router-dom";
+import { BASE_URL, PEOPLE_ENDPOINT } from "./urls.ts";
+import { useEffect } from "react";
+
 export function People() {
+  const { peopleId } = useParams();
+  console.log({ peopleId });
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(BASE_URL + PEOPLE_ENDPOINT + peopleId);
+      const data = await response.json();
+      console.log({ data });
+    }
+    getData();
+  }, [peopleId]);
+
   return <h1>PEOPLE</h1>;
 }
--- src/urls.ts
+++ src/urls.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = "https://swapi.dev/api";
 
-export const PEOPLE_ENDPOINT = "/people";
-export const STARSHIPS_ENDPOINT = "/starships";
-export const PLANETS_ENDPOINT = "/planets";
+export const PEOPLE_ENDPOINT = "/people/";
+export const STARSHIPS_ENDPOINT = "/starships/";
+export const PLANETS_ENDPOINT = "/planets/";
