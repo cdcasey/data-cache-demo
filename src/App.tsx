c72d8a5853de8df3bb7a346f4d4049cd26e53f84
Chris Casey <christopher.casey@ascension-external.org>
2024-02-01 09:15:23 -0600

    Add linting/formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,87 @@
-import { NavLink, Outlet } from "react-router-dom";
-import { useState } from "react";
+import { NavLink, Outlet } from 'react-router-dom';
+import { useState } from 'react';
 
-import "./App.css";
+import './App.css';
 
 function App() {
-  const [endpointId, setEndpointId] = useState("0");
+	const [endpointId, setEndpointId] = useState('0');
 
-  const handleButtonClick = (id: string) => {
-    setEndpointId(id);
-    // navigate(`${id}`);
-  };
+	const handleButtonClick = (id: string) => {
+		setEndpointId(id);
 
-  return (
-    <>
-      <div id="sidebar">
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to={`/People/${endpointId}`}>People</NavLink>
-            </li>
-            <li>
-              <NavLink to={`/Starships/${endpointId}`}>Starships</NavLink>
-            </li>
-            <li>
-              <NavLink to={`/Planets/${endpointId}`}>Planets</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <div>
-          <button
-            style={{
-              backgroundColor: endpointId === "1" ? "lightgrey" : undefined,
-            }}
-            onClick={() => handleButtonClick("1")}
-          >
-            1
-          </button>
-          <button
-            style={{
-              backgroundColor: endpointId === "2" ? "lightgrey" : undefined,
-            }}
-            onClick={() => handleButtonClick("2")}
-          >
-            2
-          </button>
-          <button
-            style={{
-              backgroundColor: endpointId === "3" ? "lightgrey" : undefined,
-            }}
-            onClick={() => handleButtonClick("3")}
-          >
-            3
-          </button>
-          <button
-            style={{
-              backgroundColor: endpointId === "4" ? "lightgrey" : undefined,
-            }}
-            onClick={() => handleButtonClick("4")}
-          >
-            4
-          </button>
-        </div>
-      </div>
-      <div id="detail">
-        <h1>ID: {endpointId}</h1>
-        <Outlet />
-      </div>
-    </>
-  );
+		// navigate(`${id}`);
+	};
+
+	return (
+		<>
+			<div id="sidebar">
+				<nav>
+					<ul>
+						<li>
+							<NavLink to="/">Home</NavLink>
+						</li>
+						<li>
+							<NavLink to={`/People/${endpointId}`}>
+								People
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to={`/Starships/${endpointId}`}>
+								Starships
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to={`/Planets/${endpointId}`}>
+								Planets
+							</NavLink>
+						</li>
+					</ul>
+				</nav>
+				<div>
+					<button
+						style={{
+							backgroundColor:
+								endpointId === '1' ? 'lightgrey' : undefined,
+						}}
+						onClick={() => handleButtonClick('1')}
+					>
+						1
+					</button>
+					<button
+						style={{
+							backgroundColor:
+								endpointId === '2' ? 'lightgrey' : undefined,
+						}}
+						onClick={() => handleButtonClick('2')}
+					>
+						2
+					</button>
+					<button
+						style={{
+							backgroundColor:
+								endpointId === '3' ? 'lightgrey' : undefined,
+						}}
+						onClick={() => handleButtonClick('3')}
+					>
+						3
+					</button>
+					<button
+						style={{
+							backgroundColor:
+								endpointId === '4' ? 'lightgrey' : undefined,
+						}}
+						onClick={() => handleButtonClick('4')}
+					>
+						4
+					</button>
+				</div>
+			</div>
+			<div id="detail">
+				<h1>ID: {endpointId}</h1>
+				<Outlet />
+			</div>
+		</>
+	);
 }
 
 export default App;
