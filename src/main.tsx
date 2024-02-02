9eaa843ccebe8695359afad1acb35fe6ffcb0374
Chris Casey <cdcasey@users.noreply.github.com>
2024-02-01 21:37:18 -0600

    Set up query client

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
 // import ReactDOM from 'react-dom/client'
-import ReactDOM from "react-dom";
-import { RouterProvider } from "react-router-dom";
+import ReactDOM from 'react-dom';
+import { RouterProvider } from 'react-router-dom';
 
-import { router } from "./Routes.tsx";
+import { router } from './Routes.tsx';
 // import App from "./App.tsx";
-import "./index.css";
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // ReactDOM.createRoot(document.getElementById('root')!).render(
 //   <React.StrictMode>
@@ -13,9 +14,12 @@ import "./index.css";
 //   </React.StrictMode>,
 // )
 
+const queryClient = new QueryClient();
 ReactDOM.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-  document.getElementById("root"),
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	</React.StrictMode>,
+	document.getElementById('root'),
 );
