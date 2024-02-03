ed2a66a3be54bf42bfb6c93517c49fcaa274a388
Chris Casey <cdcasey@users.noreply.github.com>
2024-02-02 23:40:02 -0600

    added types

--- src/People.tsx
+++ src/People.tsx
@@ -1,12 +1,17 @@
 import { PEOPLE_ENDPOINT } from './urls.ts';
 import { useParams } from 'react-router-dom';
-import { useData } from './useData.ts';
+
 import { usePeople } from './hooks/usePeople.ts';
 
 export function People() {
 	// const { data, isLoading } = useData({ endpoint: PEOPLE_ENDPOINT });
 	const { id } = useParams();
-	const { data, isLoading } = usePeople(PEOPLE_ENDPOINT, id);
-	console.log({ data, isLoading });
-	return <h2>Person: {isLoading ? 'LOADING...' : data?.name}</h2>;
+	const { data, isLoading, filmsSquared } = usePeople(PEOPLE_ENDPOINT, id);
+	console.log('PEOPLE');
+	console.log({ data });
+	return (
+		<h2>
+			Person: {isLoading ? 'LOADING...' : data?.name} {filmsSquared}
+		</h2>
+	);
 }
--- src/hooks/usePeople.ts
+++ src/hooks/usePeople.ts
@@ -1,13 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchData } from './fetchData.ts';
+import People from '../People.ts';
+import { useMemo } from 'react';
 
-export function usePeople(endpoint, id) {
+export function usePeople(endpoint: string, id?: string) {
 	console.log(!!id && id !== '0');
-	const { data, isLoading } = useQuery({
+	const { data, isLoading } = useQuery<People>({
 		queryKey: [endpoint, id],
 		queryFn: () => fetchData(endpoint, id),
 		enabled: !!id && id !== '0',
 	});
 
-	return { data, isLoading };
+	const filmsSquared = useMemo(() => {
+		return (data?.films.length || 0) * (data?.films.length || 0);
+	}, [data?.films.length]);
+
+	return { data, isLoading, filmsSquared };
 }
