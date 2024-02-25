d4112f90d16f2932bf27e5b6882ee22f6aeb3e84
Chris Casey <cdcasey@users.noreply.github.com>
2024-02-24 21:13:02 -0600

    add some doc string

--- src/hooks/fetchData.ts
+++ src/hooks/fetchData.ts
@@ -1,10 +1,20 @@
 import { BASE_URL } from '../urls.ts';
 
-export const fetchData = async (endpoint, id) => {
-	const response = await fetch(`${BASE_URL}${endpoint}/${id}`);
+/**
+ * Fetches data from an API endpoint.
+ * @param {string} endpoint - The API endpoint to fetch data from.
+ * @param {any} id - The ID of the resource to fetch.
+ * @returns {Promise<any>} - The JSON data from the API response.
+ * @throws {Error} - If the network response is not ok.
+ */
+export const fetchData = async (endpoint: string, id: any): Promise<any> => {
+	const url = `${BASE_URL}${endpoint}/${id}`;
+	const response = await fetch(url);
+
 	if (!response.ok) {
 		throw new Error('Network response was not ok');
 	}
+
 	console.log('FETCHING');
 	return response.json();
 };
