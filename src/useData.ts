1c33e5993f1486b61a2f669dbd695621e24ce4a5
Chris Casey <cdcasey@users.noreply.github.com>
2024-03-01 20:45:18 -0600

    new data fetching

--- src/useData.ts
+++ src/useData.ts
@@ -1,25 +1,131 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-import { BASE_URL } from "./urls.ts";
+import { BASE_URL } from './urls.ts';
 
-export function useData({ endpoint }: { endpoint: string }) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState();
-  const { id } = useParams();
+export function useGetData({ endpoint }: { endpoint: string }) {
+	const [isLoading, setIsLoading] = useState(false);
+	const [data, setData] = useState();
+	const { id } = useParams();
 
-  useEffect(() => {
-    async function getData() {
-      setIsLoading(true);
-      const response = await fetch(BASE_URL + endpoint + id);
-      const responseData = await response.json();
-      setData(responseData);
-      setIsLoading(false);
-    }
-    if (id && id !== "0") {
-      getData();
-    }
-  }, [endpoint, id]);
+	useEffect(() => {
+		async function getData() {
+			setIsLoading(true);
+			const response = await fetch(BASE_URL + endpoint + id);
+			const responseData = await response.json();
+			setData(responseData);
+			setIsLoading(false);
+		}
+		if (id && id !== '0') {
+			getData();
+		}
+	}, [endpoint, id]);
 
-  return { data, isLoading };
+	return { data, isLoading };
+}
+
+// export function useGetDataById({
+// 	endpoint,
+// 	id,
+// }: {
+// 	endpoint: string;
+// 	id: number;
+// }) {
+// 	const [isLoading, setIsLoading] = useState(false);
+// 	const [data, setData] = useState();
+
+// 	useEffect(() => {
+// 		async function getData() {
+// 			setIsLoading(true);
+// 			const response = await fetch(BASE_URL + endpoint + id);
+// 			const responseData = await response.json();
+// 			setData(responseData);
+// 			setIsLoading(false);
+// 		}
+// 		if (id && id !== '0') {
+// 			getData();
+// 		}
+// 	}, [endpoint, id]);
+
+// 	return { data, isLoading };
+// }
+
+export function useCreateData<T>({
+	endpoint,
+	body,
+}: {
+	endpoint: string;
+	body: T;
+}) {
+	const [isLoading, setIsLoading] = useState(false);
+	const [data, setData] = useState();
+
+	useEffect(() => {
+		async function createData() {
+			setIsLoading(true);
+			const response = await fetch(BASE_URL + endpoint, {
+				method: 'POST',
+				body: JSON.stringify(body),
+			});
+			const responseData = await response.json();
+			setData(responseData);
+			setIsLoading(false);
+		}
+		createData();
+	}, [endpoint, body]);
+
+	return { data, isLoading };
+}
+
+export function useUpdateData<T>({
+	endpoint,
+	body,
+}: {
+	endpoint: string;
+	body: T;
+}) {
+	const [isLoading, setIsLoading] = useState(false);
+	const [data, setData] = useState();
+
+	useEffect(() => {
+		async function updateData() {
+			setIsLoading(true);
+			const response = await fetch(BASE_URL + endpoint, {
+				method: 'PATCH',
+				body: JSON.stringify(body),
+			});
+			const responseData = await response.json();
+			setData(responseData);
+			setIsLoading(false);
+		}
+		updateData();
+	}, [endpoint, body]);
+
+	return { data, isLoading };
+}
+
+export function useDeleteData({
+	endpoint,
+	id,
+}: {
+	endpoint: string;
+	id: number;
+}) {
+	const [isLoading, setIsLoading] = useState(false);
+	const [data, setData] = useState();
+
+	useEffect(() => {
+		async function deleteData() {
+			setIsLoading(true);
+			const response = await fetch(BASE_URL + endpoint + `/${id}`, {
+				method: 'DELETE',
+			});
+			const responseData = await response.json();
+			setData(responseData);
+			setIsLoading(false);
+		}
+		deleteData();
+	}, [endpoint, id]);
+
+	return { data, isLoading };
 }
